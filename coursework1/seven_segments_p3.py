d9250e217121db0e862973579ffc1dce41e6e7dc
zhiyuli3 <48780818+zhiyuli3@users.noreply.github.com>
2020-02-19 09:47:33 +0000

    Some tests
    Some tests

--- coursework1/seven_segments_p3.py
+++ coursework1/seven_segments_p3.py
@@ -82,71 +82,73 @@ def update_synch(weight,vector,threshold,times):
                 vector[update_index] = -1 
     return vector
 
-submission=Submission("Yuli Zhi")
-submission.header("Yuli Zhi")
+# The main function
+if __name__ == '__main__':
+    submission=Submission("Yuli Zhi")
+    submission.header("Yuli Zhi")
 
-six=  [1,1,-1,1,1,1,1,-1,1,1,-1] #0110
-three=[1,-1,1,1,-1,1,1,1,1,-1,-1] #0011
-one=  [-1,-1,1,-1,-1,1,-1,1,-1,-1,-1] #0001
-seven_segment(three)
-seven_segment(six)
-seven_segment(one)
+    six=  [1,1,-1,1,1,1,1,-1,1,1,-1] #0110
+    three=[1,-1,1,1,-1,1,1,1,1,-1,-1] #0011
+    one=  [-1,-1,1,-1,-1,1,-1,1,-1,-1,-1] #0001
+    seven_segment(three)
+    seven_segment(six)
+    seven_segment(one)
 
-# Associate the patterns
-Associate = np.array([six,three,one])
-print("Associate the patterns is: ", Associate)
-weight_matrix = create_W_pattern(Associate)
-print("Store Patterns is: ", weight_matrix)
-##this assumes you have called your weight matrix "weight_matrix"
-submission.section("Weight matrix")
-submission.matrix_print("W",weight_matrix)
+    # Associate the patterns
+    Associate = np.array([six,three,one])
+    print("Associate the patterns is: ", Associate)
+    weight_matrix = create_W_pattern(Associate)
+    print("Store Patterns is: ", weight_matrix)
+    ##this assumes you have called your weight matrix "weight_matrix"
+    submission.section("Weight matrix")
+    submission.matrix_print("W",weight_matrix)
 
-print("test1")
-submission.section("Test 1")
+    print("test1")
+    submission.section("Test 1")
 
-test=[1,-1,1,1,-1,1,1,-1,-1,-1,-1]
-# Updata the net
-Test_result_1 = update_synch(weight_matrix,test,0,100)
-# Output
-print("Test_result_1: ", Test_result_1)
-print("Test_1_Seven_segment show： ")
-seven_segment(Test_result_1)
-submission.seven_segment(Test_result_1)
-##for COMSM0027
+    test1=[1,-1,1,1,-1,1,1,-1,-1,-1,-1]
+    # Updata the net
+    Test_result_1 = update_synch(weight_matrix,test1,0,100)
+    # Output
+    print("Test_result_1: ", Test_result_1)
+    print("Test_1_Seven_segment show： ")
+    seven_segment(Test_result_1)
+    submission.seven_segment(Test_result_1)
+    ##for COMSM0027
 
-##where energy is the energy of test
-#submission.print_number(energy)
+    ##where energy is the energy of test
+    #submission.print_number(energy)
 
-##this prints a space
-#submission.qquad()
+    ##this prints a space
+    #submission.qquad()
 
-#here the network should run printing at each step
-#for the final submission it should also output to submission on each step
+    #here the network should run printing at each step
+    #for the final submission it should also output to submission on each step
 
-print("test2")
+    print("test2")
 
-test2=[1,1,1,1,1,1,1,-1,-1,-1,-1]
-submission.section("Test 2")
-# Updata the net
-Test_result_2 = update_synch(weight_matrix,test2,0,100)
-print("Test_result_2: ", Test_result_2)
-print("Test_2_Seven_segment show： ")
-# Output
-seven_segment(Test_result_2)
-submission.seven_segment(Test_result_2)
+    test2=[1,1,1,1,1,1,1,-1,-1,-1,-1]
+    submission.section("Test 2")
+    # Updata the net
+    Test_result_2 = update_synch(weight_matrix,test2,0,100)
+    print("Test_result_2: ", Test_result_2)
+    print("Test_2_Seven_segment show： ")
+    # Output
+    seven_segment(Test_result_2)
+    submission.seven_segment(Test_result_2)
 
-##for COMSM0027
-##where energy is the energy of test
-#submission.print_number(energy)
+    ##for COMSM0027
+    ##where energy is the energy of test
+    #submission.print_number(energy)
 
-##this prints a space
-#submission.qquad()
+    ##this prints a space
+    #submission.qquad()
 
-#here the network should run printing at each step
-#for the final submission it should also output to submission on each step
+    #here the network should run printing at each step
+    #for the final submission it should also output to submission on each step
 
 
-submission.bottomer()
+    submission.bottomer()
 
 
 
